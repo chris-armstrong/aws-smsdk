3a1ffaf4f8d31ce6e6adee3bdd44c51c1de5229b
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2024-04-12 22:20:50 +1000

    cleanup formatting

--- awssdklib/Ini.ml
+++ awssdklib/Ini.ml
@@ -10,29 +10,29 @@ let load_ini path =
       let section : string option ref = ref None in
       let values : (string * string) list ref = ref [] in
       let push_section () =
-        if Option.is_some !section then (
+        if Option.is_some !section then begin
           sections := (!section |> Option.get, !values |> List.rev) :: !sections;
           section := None;
-          values := [])
+          values := []
+        end
       in
       Seq.iteri
         (fun line_no line ->
-          let without_comment =
-            line |> Str.split hash |> List.hd_opt |> Option.value ~default:""
-          in
-          if Str.string_match section_marker without_comment 0 then (
+          let without_comment = line |> Str.split hash |> List.hd_opt |> Option.value ~default:"" in
+          if Str.string_match section_marker without_comment 0 then begin
             push_section ();
-            section := Some (Str.matched_group 1 without_comment))
-          else if Str.string_match value_marker without_comment 0 then (
+            section := Some (Str.matched_group 1 without_comment)
+          end
+          else if Str.string_match value_marker without_comment 0 then begin
             if Option.is_none !section then
-              raise
-                (InvalidFormat
-                   (Fmt.str "value without section on line %d" line_no));
+              raise (InvalidFormat (Fmt.str "value without section on line %d" line_no));
             let key = Str.matched_group 1 without_comment |> String.trim in
             let value = Str.matched_group 2 without_comment |> String.trim in
-            values := (key, value) :: !values)
-          else if not (without_comment |> String.trim |> String.length = 0) then
-            raise (InvalidFormat (Fmt.str "could not parse line %d" line_no)))
+            values := (key, value) :: !values
+          end
+          else if not (without_comment |> String.trim |> String.length = 0) then begin
+            raise (InvalidFormat (Fmt.str "could not parse line %d" line_no))
+          end)
         lines;
       push_section ();
       !sections |> List.rev)
