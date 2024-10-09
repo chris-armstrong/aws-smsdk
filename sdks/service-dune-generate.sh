84d998b9027f42d7b286a872d5f3d97551d570f6
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2024-10-10 09:30:17 +1100

    chore: service dune file generator

---
+++ sdks/service-dune-generate.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+for dir in *
+do
+	if [ -d "${dir}" ] && [ ! -f "${dir}/dune" ]
+	then
+		service_short_name=$(basename $dir)
+		service_long_name=$(grep "cloudFormationName" ../awssdkjsv3/codegen/sdk-codegen/aws-models/${service_short_name}.json | sed -E -e 's/.*"cloudFormationName": "(.*)".*/\1/')
+		if [ -z "${service_long_name}" ]; then
+			service_long_name=$(grep "sdkId" ../awssdkjsv3/codegen/sdk-codegen/aws-models/${service_short_name}.json | sed -E -e 's/.*"sdkId": "(.*)".*/\1/' -e 's/ //g')
+		fi
+		if [ -z "${service_long_name}" ]; then
+			echo "Unable to generate for $service_short_name!"
+			exit 1
+		fi
+		echo "Generating "${dir}/dune" for \"$service_short_name\" \"$service_long_name\""
+		cat service-dune-template| sed -e "s/%%service-short-name%%/${service_short_name}/g"  -e "s/%%service-full-name%%/${service_long_name}/g"  > "${dir}/dune"
+	fi
+done
